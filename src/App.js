edbb634b29e07eb61e31368e146f7a5b595419e4
vn03dz9 <niharika.gandhari@samsclub.com>
2020-10-22 16:39:06 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,73 +1,93 @@
 import React, { Component } from 'react';
 import './App.css';
-import moment from 'moment';
 
 class App extends Component {
 
-  state={
-    hour: 0,
+  state = {
+    hours: 0,
     minutes: 0,
-    seconds:0 
-  
+    seconds:0
   }
 
-  inputHandler = (e) =>{  
-    console.log(e.target.name);
+  inputHandler = (e) => {
+    e.stopPropagation();
     this.setState({[e.target.name]: e.target.value});
   }
 
+  convertToSeconds = ( hours, minutes,seconds) => {
+    return seconds + minutes * 60 + hours * 60 * 60;
+  }
+
   startTimer = () => {
+    this.timer = setInterval(this.countDown, 1000);
+  }
 
-    if(this.state.hour!==0||this.state.minutes!==0||this.state.seconds!==0){
-      if(this.state.seconds!==0&&this.state.minutes===0){
-      this.timer=setInterval(() => {
-        this.setState({seconds:this.state.seconds-1}) 
-        if(this.state.seconds===0){clearInterval(this.timer)};
-      }, 1000);}
+  countDown = () => {
+    const  { hours, minutes, seconds } = this.state;
+    let c_seconds = this.convertToSeconds(hours, minutes, seconds);
 
-      if(this.state.minutes!==0&&this.state.seconds===0){
-        this.setState({seconds:60});
-        this.timer=setInterval(() => {
-          this.setState({seconds:this.state.seconds-1})
-          if(this.state.seconds===0){
-            if(this.state.minutes!==0){
-              this.setState({minutes:this.state.minutes-1, seconds:60});
-            }
-            else{clearInterval(this.timer)}
-          }
-        },1000)
+    if(c_seconds) {
+
+      // seconds change
+      seconds ? this.setState({seconds: seconds-1}) : this.setState({seconds: 59});
+
+      // minutes change
+      if(c_seconds % 60 === 0 && minutes) {
+        this.setState({minutes: minutes -1});
       }
 
+      // when only hours entered
+      if(!minutes && hours) {
+        this.setState({minutes: 59});
+      }
+
+      // hours change
+      if(c_seconds % 3600 === 0 && hours) {
+        this.setState({hours: hours-1});
+      }
+
+    } else {
+      clearInterval(this.timer);
     }
-   
-    console.log("start");
   }
-  stopTimer  = () => {
+
+
+  stopTimer = () => {
     clearInterval(this.timer);
-    console.log("stop")
   }
-  resetTimer  = () => {
-    this.setState({hour:0, minutes:0, seconds:0});
-    console.log("reset")
+
+  resetTimer = () => {
+    this.setState({
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    });
+    this.hoursInput.value = 0;
+    this.minutesInput.value = 0;
+    this.secondsInput.value = 0;
   }
 
 
   render() {
+    const { hours, minutes, seconds } = this.state;
+
     return (
       <div className="App">
          <h1 className="title"> (( React Countdown )) </h1>
          <div className="inputGroup">
+            <h3>Hrs</h3>
+            <input ref={el => this.hoursInput = el} type="number" placeholder={0}  name="hours"  onChange={this.inputHandler} />
             <h3>Min</h3>
-            <input type="number"  value={this.state.minutes}   name="minutes"  onChange={(e) => this.inputHandler(e)} />
+            <input  ref={el => this.minutesInput = el} type="number"  placeholder={0}   name="minutes"  onChange={this.inputHandler} />
             <h3>Sec</h3>
-            <input type="number"  value={this.state.seconds}   name="seconds"  onChange={(e) => this.inputHandler(e)} />
+            <input   ref={el => this.secondsInput = el} type="number"  placeholder={0}  name="seconds"  onChange={this.inputHandler} />
          </div>
          <div>
             <button onClick={this.startTimer} className="start">start</button>
             <button onClick={this.stopTimer}  className="stop">stop</button>
             <button onClick={this.resetTimer}  className="reset">reset</button>
          </div>
-         <h1> Timer {this.state.minutes} : {this.state.seconds} </h1>
+         <h1> Timer {hours}: {minutes} : {seconds} </h1>
       </div>
 
     );
