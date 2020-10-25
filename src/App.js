1da89dc08067fefd3bbe2c7a4dd27e32448610fe
vn03dz9 <niharika.gandhari@samsclub.com>
2020-10-24 17:21:49 -0700

    implement via hooks

--- src/App.js
+++ src/App.js
@@ -1,101 +1,98 @@
-import React, { Component } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      hours: 0,
-      minutes: 0,
-      seconds:0
-    }
-    this.hoursInput = React.createRef();
-    this.minutesInput= React.createRef();
-    this.secondsInput = React.createRef();
+const App = () => {
+
+  const timerRef = useRef();
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const latestSeconds = useRef(seconds);
+  const latestMinutes = useRef(minutes);
+  const latestHours = useRef(hours);
+
+  const hoursRef = useRef();
+  const minutesRef = useRef();
+  const secondsRef = useRef();
+
+  const convertToSeconds = () => {
+    return latestSeconds.current + latestMinutes.current * 60 + latestHours.current * 60 * 60;
+  };
+
+  const startTimer = () => {
+    timerRef.current = setInterval(countDown, 1000);
   }
 
-  inputHandler = (e) => {
-    this.setState({[e.target.name]: e.target.value});
-  }
-
-  convertToSeconds = ( hours, minutes,seconds) => {
-    return seconds + minutes * 60 + hours * 60 * 60;
-  }
-
-  startTimer = () => {
-    this.timer = setInterval(this.countDown, 1000);
-  }
-
-  countDown = () => {
-    const  { hours, minutes, seconds } = this.state;
-    let c_seconds = this.convertToSeconds(hours, minutes, seconds);
-
+  const countDown = () => {
+    let c_seconds = convertToSeconds();
     if(c_seconds) {
 
       // seconds change
-      seconds ? this.setState({seconds: seconds-1}) : this.setState({seconds: 59});
+      if(latestSeconds.current) {
+        latestSeconds.current = latestSeconds.current-1;
+        setSeconds(seconds => seconds-1);
+      } else {
+        latestSeconds.current = 59;
+        setSeconds(59);
+      }
 
       // minutes change
-      if(c_seconds % 60 === 0 && minutes) {
-        this.setState({minutes: minutes -1});
+      if(c_seconds % 60 === 0 && latestMinutes.current) {
+        latestMinutes.current = latestMinutes.current-1;
+        setMinutes(minutes => minutes-1)
       }
 
       // when only hours entered
-      if(!minutes && hours) {
-        this.setState({minutes: 59});
+      if(!minutes && latestHours.current) {
+        latestMinutes.current = 59;
+        setMinutes(59);
       }
 
       // hours change
-      if(c_seconds % 3600 === 0 && hours) {
-        this.setState({hours: hours-1});
+      if(c_seconds % 3600 === 0 && latestHours.current) {
+        latestHours.current = latestHours.current-1;
+        setHours(hours => hours-1);
       }
 
     } else {
-      clearInterval(this.timer);
+      clearInterval(timerRef.current);
     }
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerRef.current);
   }
 
+  const resetTimer = () => {
+    setSeconds(0);
+    setMinutes(0);
+    setHours(0);
 
-  stopTimer = () => {
-    clearInterval(this.timer);
+    hoursRef.current.value = 0;
+    minutesRef.current.value = 0;
+    secondsRef.current.value = 0;
   }
 
-  resetTimer = () => {
-    this.setState({
-      hours: 0,
-      minutes: 0,
-      seconds: 0
-    });
-    this.hoursInput.current.value = 0;
-    this.minutesInput.current.value = 0;
-    this.secondsInput.current.value = 0;
-  }
-
-
-  render() {
-    const { hours, minutes, seconds } = this.state;
-
-    return (
-      <div className="App">
-         <h1 className="title"> (( React Countdown )) </h1>
-         <div className="inputGroup">
-            <h3>Hrs</h3>
-            <input ref={this.hoursInput} type="number" placeholder={0}  name="hours"  onChange={this.inputHandler} />
-            <h3>Min</h3>
-            <input  ref={this.minutesInput} type="number"  placeholder={0}   name="minutes"  onChange={this.inputHandler} />
-            <h3>Sec</h3>
-            <input   ref={this.secondsInput} type="number"  placeholder={0}  name="seconds"  onChange={this.inputHandler} />
-         </div>
-         <div>
-            <button onClick={this.startTimer} className="start">start</button>
-            <button onClick={this.stopTimer}  className="stop">stop</button>
-            <button onClick={this.resetTimer}  className="reset">reset</button>
-         </div>
-         <h1> Timer {hours}: {minutes} : {seconds} </h1>
+  return(
+    <div className="App">
+      <h1 className="title"> (( React Countdown )) </h1>
+      <div className="inputGroup">
+        <h3>Hrs</h3>
+        <input ref={hoursRef} type="number" placeholder={0}  name="hours"  onChange={(e) => {latestHours.current = Number(e.target.value); setHours(Number(e.target.value))}} />
+        <h3>Min</h3>
+        <input  ref={minutesRef} type="number"  placeholder={0}   name="minutes"  onChange={(e) => {latestMinutes.current = Number(e.target.value);setMinutes(Number(e.target.value))}} />
+        <h3>Sec</h3>
+        <input   ref={secondsRef} type="number"  placeholder={0}  name="seconds"  onChange={(e) => {latestSeconds.current = Number(e.target.value); setSeconds(Number(e.target.value))}} />
       </div>
-
-    );
-  }
+      <div>
+        <button onClick={startTimer} className="start">start</button>
+        <button onClick={stopTimer}  className="stop">stop</button>
+        <button onClick={resetTimer}  className="reset">reset</button>
+      </div>
+      <h1> Timer {hours}: {minutes} : {seconds} </h1>
+    </div>
+  );
 }
 
 export default App;
